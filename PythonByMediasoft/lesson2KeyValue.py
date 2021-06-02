26a742e29b71baa7b206557f6969fef8f5898e4f
Sunsmailer <44652460+Sunsmailer@users.noreply.github.com>
2021-06-02 15:33:23 +0400

    Update lesson2KeyValue.py

--- PythonByMediasoft/lesson2KeyValue.py
+++ PythonByMediasoft/lesson2KeyValue.py
@@ -1,3 +1,16 @@
+#первый вариант
+from collections import *
+slovar = defaultdict(list)
+dicts = {'name': 'ivan', 'surname': 'ivanov'}
+for key, value in dicts.items():
+    slovar[value].append(key)
+print(slovar)
+
+
+
+
+
+#Второй вариант
 slovar = {'name': 'ivan', 'surname': 'ivanov'}
 new_slovar ={}
 for key, value in slovar.items():
